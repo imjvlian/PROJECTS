03f49c70e1706bbe3f0956c18e6a326e38fa618c
jvlian <julianvikbusiness@gmail.com>
2025-11-03 05:38:57 +0700

    0.0.7-2

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
     const [visible, setVisible] = useState(false);
 
   return (
-    <div className="fixed top-0 right-0 left-0 z-50 bg-gray-200 dark:bg-gray-800 flex items-center justify-between px-5 py-5 font-medium shadow-md">
+    <div className="lg:fixed right-25 left-25 z-50 full bg-gray-200 dark:bg-gray-800 flex items-center justify-between px-5 py-5 font-medium shadow-md">
       <Link to='/'><img src={assets.logo} className="w-36 cursor-pointer" alt="" /></Link>
       
 
@@ -34,15 +34,15 @@ const Navbar = () => {
         </NavLink>
       </ul>
 
-      <div className="flex items-center gap-6">
+      <div className="flex items-center gap-3 lg:gap-6">
         <IoIosSearch size={35} className="w-5 cursor-pointer text-gray-700 dark:text-white" />
         <div className="group relative">
           <HiOutlineUser size={35} className="w-5 cursor-pointer text-gray-700 dark:text-white"/>
           <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
-            <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
-              <p className="cursor-pointer hover:text-black">My Profile</p>
-              <p className="cursor-pointer hover:text-black">Orders</p>
-              <p className="cursor-pointer hover:text-black">Logout</p>
+            <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 dark:bg-gray-900 text-gray-500 dark:text-gray-400 rounded shadow-md">
+              <p className="cursor-pointer dark:hover:text-white hover:text-black">My Profile</p>
+              <p className="cursor-pointer dark:hover:text-white hover:text-black">Orders</p>
+              <p className="cursor-pointer dark:hover:text-white hover:text-black">Logout</p>
             </div>
           </div>
         </div>
@@ -61,8 +61,8 @@ const Navbar = () => {
       </div>
 
         {/* Mobile Menu */}
-        <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? 'w-full' : 'w-0'}`}>
-            <div className="flex flex-col text-gray-600">
+        <div className={`absolute top-0 right-0 bottom-0 z-50  overflow-hidden bg-white dark:bg-gray-800 transition-all  ${visible ? 'w-full' : 'w-0'}`}>
+            <div className="flex flex-col text-gray-600 dark:text-gray-200">
                 <div onClick={()=>setVisible(false)} className="flex items-center gap-4 p-3 cursor-pointer">
                     <img className="h-4 rotate-180" src={assets.dropdown_icon} alt="" />
                     <p>Back</p>
